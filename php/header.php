9eac33264f28a85a8b0f042ee157a53017acacc4
Bosona19 <161635012+Bosona19@users.noreply.github.com>
2024-05-30 00:16:51 +0300

    update header

--- php/header.php
+++ php/header.php
@@ -1,32 +1,52 @@
-<nav class="navbar navbar-inverse navabar-fixed-top">
-               <div class="container">
-                   <div class="navbar-header">
-                       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                           <span class="icon-bar"></span>
-                           <span class="icon-bar"></span>
-                           <span class="icon-bar"></span>
-                       </button>
-                       <a href="index.php" class="navbar-brand">Store</a>
-                   </div>
-                   
-                   <div class="collapse navbar-collapse" id="myNavbar">
-                       <ul class="nav navbar-nav navbar-right">
-                           <?php
-                           if(isset($_SESSION['email'])){
-                           ?>
-                           <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
-                           <li><a href="settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
-                           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-                           <?php
-                           }else{
-                            ?>
-                            <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                           <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                           <?php
-                           }
-                           ?>
-                           
-                       </ul>
-                   </div>
-               </div>
-</nav>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Store</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="index.php" class="navbar-brand">Store</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if (isset($_SESSION['email'])): ?>
+                        <li class="inline"><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                        <li class="inline bsg">
+                            <img id="user_image" src="./upload/<?php echo $photo_url ?>" alt="">
+                        </li>
+                        <li class="dropdown">
+                            <button class="dropbtn" style="font-weight:bold;">
+                                <?php echo 'HELLO' //ucwords($name); ?>
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-content">
+                                <a href="#"></a>
+                                <a href="profile.php">Update profile</a>
+                                <a href="settings.php">Settings</a>
+                                <a href="logout.php">Logout</a>
+                            </div>
+                        </li>
+                    <?php else: ?>
+                        <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="bootstrap/js/jquery-3.2.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
